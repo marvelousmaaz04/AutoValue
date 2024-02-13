63ee626cf14f06117480ea7bd47516559933ac04
marvelousmaaz04 <shaikhmaaz04@gmail.com>
2024-02-13 19:56:36 +0530

    deployment

---
+++ Deployment_Iteration/Final_Year_Project/AutoValue/website/Models.py
@@ -0,0 +1,21 @@
+from . import db
+from flask_login import UserMixin
+from datetime import datetime, timedelta
+import secrets
+
+class Users(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    full_name = db.Column(db.String(), nullable=False) 
+    email = db.Column(db.String(),unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    reset_token = db.Column(db.String(120), unique=True, nullable=True)
+    reset_token_expiration = db.Column(db.DateTime, nullable=True)
+
+    def generate_reset_token(self):
+        # Generate a secure random token with 32 bytes
+        self.reset_token = secrets.token_urlsafe(32)
+        self.reset_token_expiration = datetime.utcnow() + timedelta(minutes=5)
+
+class Subscribers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
---
+++ Deployment_Iteration/Final_Year_Project/AutoValue/website/static/js/blogs.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(".filter-item").click(function(){
+        const value = $(this).attr("data-filter");
+        if(value == "all"){
+            $(".post-box").show("1000");
+
+        }else{
+            $(".post-box").not("."+value).hide("1000");
+            $(".post-box").filter("."+value).show("1000");
+        }
+    })
+
+    $(".filter-item").click(function(){
+        $(this).addClass("active-filter").siblings().removeClass("active-filter")
+    })
+})
+
+
+// Initialize Swiper
+var swiper = new Swiper(".banner-slider", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+
+        },
+        768: {
+            slidesPerView: 1,
+
+        },
+        1024: {
+            slidesPerView: 1,
+
+        },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
